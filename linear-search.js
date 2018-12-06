3516667fadf1980390559e0220a04aa1d9c50072
Jhedi Ayala <jhedi@Jhedis-MacBook-Pro.local>
2018-12-06 13:55:17 -0500

    simple linear search

--- linear-search.js
+++ linear-search.js
@@ -1,3 +1,12 @@
 
 
 // **************
+
+const linearSearch = (arr, val) => {
+    for (let i = 0; i < arr.length; i ++){
+        if (arr[i] === val) return i;
+    }
+    return -1
+}
+
+console.log(linearSearch([3,5,1,6,5,3,], 1))
